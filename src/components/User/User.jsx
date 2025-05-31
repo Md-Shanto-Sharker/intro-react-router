631aa6ec4f003d78ee3728a22c228485cfeda84b
SHANTO <rabbymd55555@gmail.com>
2025-05-31 12:04:30 +0600

    react router recap

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import UserDetails from "../UserDetails/UserDetails";
 
 const User = ({ user }) => {
-  const { name, email, phone } = user;
+  const { name, email, phone ,id} = user;
+
+  const navigate = useNavigate()
+  const handleNavigate =()=>{
+      navigate(`/user/${user.id}`)
+  }
   return (
     <div
       style={{
@@ -19,6 +24,8 @@ const User = ({ user }) => {
       </p>
 
       <Link to={`/user/${user.id}`}> show Details</Link>
+
+      <button onClick={handleNavigate}>Details of : {id}</button>
     </div>
   );
 };
--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
 
 const UserDetails = () => {
     const data = useLoaderData();
     console.log(data);
     const {website,name} = data;
+        const navigate = useNavigate()
     
-
+   
     return (
         <div>
             <h1>User details here</h1>
             <p>Website : {website}</p>
             <p>Name : {name}</p>
+
+            <button onClick={()=>navigate(-1)}>Go Back</button>
+           
         </div>
     );
 };
