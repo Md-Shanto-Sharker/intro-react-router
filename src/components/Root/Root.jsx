90e4ac91b5ce1f36b020493be3a79ed71b85293e
SHANTO <rabbymd55555@gmail.com>
2025-05-31 10:01:14 +0600

    first commit

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from '../Header/Header';
+import { Outlet } from 'react-router';
+import Footer from '../Footer/Footer';
+import Sidebar from '../Sidebar/Sidebar';
+
+const Root = () => {
+    return (
+        <div>
+            <Header></Header>
+            <div style={
+                {display:'flex',
+                    gap:'30px'
+                }
+            }>
+                <Sidebar></Sidebar>
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
