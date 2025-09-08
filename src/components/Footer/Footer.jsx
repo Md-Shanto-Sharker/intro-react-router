da3577ff9afe9bdf5a0df8a1a0d508bab04695f0
SHANTO <rabbymd55555@gmail.com>
2025-09-08 23:30:14 +0600

    added react router

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>
+        <small>Thank you for visiting our website</small>
+        <a href="/">terms</a>
+        <a href="/">Privacy</a>
+        <a href="/fb">FaceBook</a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
